6286e7bc5d11ba759964bea1e6cf0023b5724fa7
Caden OKellylee <bmctilton@gmail.com>
2024-01-28 18:13:23 -0700

    commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+document.getElementById("in").addEventListener("keyup",(evt) => evt.key == "Enter" ? input(): null)
+function input() {
+    var content = document.getElementById("in").value;
+    document.getElementById("in").value = "";
+    // Add Code to deal w/ content later
+}
